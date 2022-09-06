d22ebb88ce7b4b1579d3cc25ca9742bb0afe0786
Anna Montero <montero.katreena@gmail.com>
2022-09-06 22:04:26 +0400

    Add and style feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = () => {
-  return <div>Feature</div>;
+const Feature = ({ number, title, description }) => {
+  return (
+    <div className="nuvola__feature">
+      <div className="nuvola__feature-col">
+        <h1 className="nuvola__feature-title--number h1-title">{number}</h1>
+      </div>
+      <div className="nuvola__feature-col">
+        <h2 className="nuvola__feature-title h2-title">{title}</h2>
+        <p className="nuvola__feature-text nuvola-text">{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import './features.css';
+import Feature from '../../components/feature/Feature';
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="nuvola__features">
+      <div className="nuvola__features-content section-padding">
+        <div className="nuvola__features-content__title h1-title">How it works</div>
+        <Feature
+          number="01"
+          title="lorem ipsum dolor sit amet"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+        />
+        <Feature
+          number="02"
+          title="lorem ipsum dolor sit amet"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+        />
+        <Feature
+          number="03"
+          title="lorem ipsum dolor sit amet"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+        />
+        <Feature
+          number="04"
+          title="lorem ipsum dolor sit amet"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Features;
