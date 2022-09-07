3764b0848618e0e742880d0133261fc941c2c84e
Anna Montero <montero.katreena@gmail.com>
2022-09-07 23:29:57 +0400

    Create layout and add swiper library for gallery section

--- src/containers/gallery/Gallery.jsx
+++ src/containers/gallery/Gallery.jsx
@@ -1,8 +1,62 @@
 import React from 'react';
 import './gallery.css';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper';
+import {
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  gallery05,
+  gallery06,
+} from './imports';
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div className="nuvola__gallery">
+      <div className="nuvola__gallery-content section__padding">
+        <div className="nuvola__gallery-content__title h1-title">
+          Lorem ipsum dolor sit amet
+        </div>
+        <p className="nuvola__gallery-content__description nuvola__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+        </p>
+      </div>
+      <div className="nuvola__gallery-slider">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          loop={true}
+          centeredSlides={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={gallery02} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={gallery04} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={gallery06} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
---
+++ src/containers/gallery/imports.js
@@ -0,0 +1,8 @@
+import gallery01 from '../../assets/gallery/gallery-01.jpeg';
+import gallery02 from '../../assets/gallery/gallery-02.png';
+import gallery03 from '../../assets/gallery/gallery-03.jpeg';
+import gallery04 from '../../assets/gallery/gallery-04.png';
+import gallery05 from '../../assets/gallery/gallery-05.jpeg';
+import gallery06 from '../../assets/gallery/gallery-06.png';
+
+export { gallery01, gallery02, gallery03, gallery04, gallery05, gallery06 };
