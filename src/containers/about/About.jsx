b177da54975ab1c0d5ce803d3e7f289cd53b900f
Anna Montero <montero.katreena@gmail.com>
2022-09-13 22:20:14 +0400

    Organized code for particles in about section

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -14,18 +14,40 @@ const About = () => {
     <div className="nuvola__about" id="about">
       <div className="nuvola__about-content section__padding">
         <div className="nuvola__about-content-col col--one">
-          <div className="wrapper">
+          <div className="nuvola__wrapper">
             <div className="nuvola__about-content__circle-bg"></div>
-            <div className="nuvola__about-blobs">
-              <img src={blobPurple02} alt="" className="blob blob-blob-purple" />
-              <img src={circlePurple} alt="" className="blob blob-circle-purple" />
+            <div className="nuvola__particles">
+              <img
+                src={blobPink02}
+                alt=""
+                className="particle particle--blob"
+                id="particle__about--blob-pink"
+              />
+              <img
+                src={blobPurple02}
+                alt=""
+                className="particle particle--blob"
+                id="particle__about--blob-purple"
+              />
+              <img
+                src={circlePurple}
+                alt=""
+                className="particle particle--circle"
+                id="particle__about--circle-purple"
+              />
               <img
                 src={circlePinkOutline}
                 alt=""
-                className="blob blob-circle-pink-outline"
+                className="particle particle--circle"
+                id="particle__about--circle-pink-outline"
               />
-              <img src={blobPink02} alt="" className="blob blob-blob-pink" />
-              <img src={circlePink} alt="" className="blob blob-circle-purple-01" />
+
+              <img
+                src={circlePink}
+                alt=""
+                className="particle particle--circle"
+                id="particle__about--circle-pink"
+              />
             </div>
             <img src={aboutNuvola} alt="" className="nuvola__about-content__img" />
           </div>
